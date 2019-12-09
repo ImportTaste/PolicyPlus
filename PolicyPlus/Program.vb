1028f2962791be25c46661bf6359af0811a5d40f
David Müller <muellerdavid4@gmail.com>
2019-12-09 01:25:53 +0100

    changed to startup with custom sub main

---
+++ PolicyPlus/Program.vb
@@ -0,0 +1,8 @@
+﻿Module Program
+    <STAThread>
+    Sub Main(args As String())
+        Application.EnableVisualStyles()
+        Application.SetCompatibleTextRenderingDefault(False)
+        Application.Run(New Main)
+    End Sub
+End Module
