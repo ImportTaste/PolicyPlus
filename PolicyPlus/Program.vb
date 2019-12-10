c7a80d87efed775f8a53fb3a66b253c6caae9967
David Müller <muellerdavid4@gmail.com>
2019-12-10 21:57:02 +0100

    started work with commandlineutils

--- PolicyPlus/Program.vb
+++ PolicyPlus/Program.vb
@@ -1,18 +1,39 @@
-﻿Imports System.Runtime.Remoting.Messaging
+﻿
+Imports System.DirectoryServices.ActiveDirectory
+Imports McMaster.Extensions.CommandLineUtils
 
 Module Program
     <STAThread>
-    Sub Main(args As String())
-        If args.Length <= 0 Then
-            Application.EnableVisualStyles()
-            Application.SetCompatibleTextRenderingDefault(False)
-            Application.Run(New Main)
-        Else
+    Function Main(args As String()) As Integer
+        Dim app = New CommandLineApplication
+        app.Name = "policyplus"
+        app.FullName = "Policy Plus"
+        app.HelpOption()
 
-            Dim parameters = args.Select(Function(x) x.Substring(1).Split("=")).ToDictionary(Function(y) y(0), Function(y) y(1))
-            Dim commandLineOptions = New CommandLineOptions(parameters("input"), parameters("output"), parameters("policysection"))
-        End If
+        Dim convert As CommandOption = app.Option("-c|--convert", "Convert file format", CommandOptionType.NoValue)
+        Dim input As CommandArgument = app.Argument("<InputFilePath>", "Path to the input file.")
+        Dim output As CommandArgument = app.Argument("<OutputFilePath>", "Path to the output file.")
+        Dim policySection As CommandArgument = app.Argument("<PolicySection>", "Section of the policy.")
 
+        'Dim parameters = args.Select(Function(x) x.Substring(1).Split("=")).ToDictionary(Function(y) y(0), Function(y) y(1))
+        'Dim commandLineOptions = New CommandLineOptions(parameters("input"), parameters("output"), parameters("policysection"))
 
-    End Sub
+        app.OnExecute(Sub()
+                          If Not convert.HasValue() Then
+                              Application.EnableVisualStyles()
+                              Application.SetCompatibleTextRenderingDefault(False)
+                              Application.Run(New Main)
+                          Else
+                              Console.WriteLine("Hello World")
+                          End If
+                      End Sub)
+
+        Try
+            Return app.Execute(args)
+        Catch ex As CommandParsingException
+            app.ShowHelp()
+            Return 102
+        End Try
+
+    End Function
 End Module
